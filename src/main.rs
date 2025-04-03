1a9858a672668bf6eb60d3f991c6270d12fc9a71
jas-ren <78553107+jas-ren@users.noreply.github.com>
2025-04-03 15:38:33 -0400

    game works

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::{collections::HashMap, hash::Hash};
 use std::cmp::min;
 use std::io;
+use rand::Rng;
+use std::fmt;
 
 
 const BOARD_WIDTH: u8 = 4;
 
-#[derive(Eq, PartialEq, Hash)]
+#[derive(Eq, PartialEq, Hash, Debug)]
 enum Color {
     Red,
     Green,
@@ -62,17 +64,69 @@ impl Row {
     }
 }
 
+impl fmt::Display for Row {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s: String = self.pegs.iter().map(|c| match c {
+                Color::Red => "R",
+                Color::Green => "G",
+                Color::Blue => "B",
+                Color::Yellow => "Y",
+                Color::Purple => "P",
+                Color::Brown => "W"
+            }).collect();
+
+        write!(f, "{}", s)
+    }
+}
+
 fn main() {
     println!("Welcome to Mastermind! How many guesses would you like?");
     let mut num_guesses = String::new();
     io::stdin().read_line(&mut num_guesses).expect("Failed to read number of guesses.");
-    let num_guesses = u32::from_str_radix(&num_guesses, 10).unwrap();
+    let num_guesses = num_guesses.trim().parse::<u32>().unwrap();
     println!("You have {} guesses!", num_guesses);
 
-    let solution = todo!();
+    let mut solution = Vec::new();
+    let mut rng = rand::rng();
 
-    for i in 0..num_guesses {
-        todo!()
+    for _ in 0..BOARD_WIDTH {
+        let new_peg = match rng.random_range(0..6) {
+            0 => Color::Red,
+            1 => Color::Green,
+            2 => Color::Blue,
+            3 => Color::Yellow,
+            4 => Color::Purple,
+            5 => Color::Brown,
+            _ => unreachable!()
+        };
+        solution.push(new_peg);
     }
+    let solution_row = Row {pegs: solution};
+
+    for _ in 0..num_guesses {
+        println!("Input Format: R=Red, G=Green, B=Blue, Y=Yellow, P=Purple, W=Brown");
+        println!("Input your guess:");
+        let mut guess_text = String::new();
+        io::stdin().read_line(&mut guess_text).expect("Error reading the guess");
+        let guess_row = Row { pegs: guess_text.trim().chars().map(|c| match c {
+            'R' | 'r' => Color::Red,
+            'G' | 'g' => Color::Green,
+            'B' | 'b' => Color::Blue,
+            'Y' | 'y' => Color::Yellow,
+            'P' | 'p' => Color::Purple,
+            'W' | 'w' => Color::Brown,
+            _ => unreachable!()
+        }).collect() };
+        let black_count = guess_row.black_peg(&solution_row);
+        println!("black pegs: {black_count}");
+        if black_count == BOARD_WIDTH as usize {
+            println!("You win!");
+            return;
+        }
+        let white_count = guess_row.white_peg(&solution_row);
+        println!("white pegs: {white_count}");
+    }
+    println!("You've run out of guesses :(");
+    println!("The correct solution is {}", solution_row);
     
 }
